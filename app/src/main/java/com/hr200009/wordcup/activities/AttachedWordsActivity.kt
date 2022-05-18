dc5be0476cb2b1572a14a518a917cbd138c7fa98
oguzhan <oguzhangunduz1994@gmail.com>
2022-05-18 18:18:45 +0300

    oyanış için ilk adım

--- app/src/main/java/com/hr200009/wordcup/activities/AttachedWordsActivity.kt
+++ app/src/main/java/com/hr200009/wordcup/activities/AttachedWordsActivity.kt
@@ -1,6 +1,10 @@
 package com.hr200009.wordcup.activities
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Button
+import android.widget.EditText
 
 import android.widget.TextView
 import android.widget.Toast
@@ -26,15 +30,16 @@ class AttachedWordsActivity : AppCompatActivity() {
     private lateinit var arrayList: ArrayList<Word>
 
 
-    private  var textSource: TextView? = null
-    private  var textTarget: TextView? = null
-    private  var textTrueCount: TextView? = null
-    private  var textFalseCount: TextView? = null
-    private  var textPassCount: TextView? = null
+    private lateinit var textSource: EditText
+    private lateinit var textTarget: EditText
+    private var textTrueCount: TextView? = null
+    private var textFalseCount: TextView? = null
+    private var textPassCount: TextView? = null
     private var textViewCount: TextView? = null
     private var textIsItLearned: TextView? = null
+    private lateinit var editButton: Button
+    private lateinit var writeButton: Button
 
-    private lateinit var text: String
 
     private lateinit var tempLayout2: ConstraintLayout
 
@@ -52,6 +57,7 @@ class AttachedWordsActivity : AppCompatActivity() {
 
 
 
+
         recyclerView = findViewById(R.id.recyclerViewAttachedWords)
         recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerView.setHasFixedSize(true)
@@ -59,33 +65,48 @@ class AttachedWordsActivity : AppCompatActivity() {
         arrayList = arrayListOf<Word>()
         run()
     }
-    private fun secondaryLayout(text: Word){
+
+    private fun secondaryLayout(text: Word) {
         setContentView(R.layout.word_detail)
-        textSource = findViewById(R.id.textView4)
-        textTarget = findViewById(R.id.textView5)
-        textTrueCount = findViewById(R.id.textView6)
-        textFalseCount = findViewById(R.id.textView7)
-        textPassCount = findViewById(R.id.textView8)
-        textViewCount = findViewById(R.id.textView9)
-        textIsItLearned = findViewById(R.id.textView10)
 
-        textSource?.text = text.source
-        textTarget?.text = text.translation
-        textTrueCount?.text = text.trueCounter.toString()
-        textFalseCount?.text = text.falseCounter.toString()
-        textPassCount?.text = text.passCounter.toString()
-        textViewCount?.text = text.viewCounter.toString()
-        if (text.isItLearned== true) {
-            textIsItLearned?.text = "Evet"
-        } else {
-            textIsItLearned?.text = "Hayır"
+        textSource = findViewById(R.id.textSource)
+        textTarget = findViewById(R.id.textTarget)
+        textTrueCount = findViewById(R.id.textTrueCount)
+        textFalseCount = findViewById(R.id.textFalseCount)
+        textPassCount = findViewById(R.id.textPassCount)
+        textViewCount = findViewById(R.id.textViewCount)
+        textIsItLearned = findViewById(R.id.textIsItLearned)
+        editButton = findViewById(R.id.editButton)
+        writeButton = findViewById(R.id.writeButton)
+
+        textSource?.setText(text.source)
+        textTarget?.setText(text.translation)
+        textTrueCount?.setText(text.trueCounter.toString())
+        textFalseCount?.setText(text.falseCounter.toString())
+        textPassCount?.setText(text.passCounter.toString())
+        textViewCount?.setText(text.viewCounter.toString())
+        textIsItLearned?.setText(text.viewCounter.toString())
+
+
+        textSource.isFocusableInTouchMode = false
+        textTarget.isFocusableInTouchMode = false
+
+        editButton.setOnClickListener() {
+            textSource.isFocusableInTouchMode = true
+            textTarget.isFocusableInTouchMode = true
+
         }
-
-
+        writeButton.setOnClickListener() {
+            database = FirebaseDatabase.getInstance().getReference("words").child(auth.uid.toString())
+                .child("allWords")
+            database.child(text.id.toString()).child("source").setValue(textSource.text.toString())
+            database.child(text.id.toString()).child("translation").setValue(textTarget.text.toString())
+        }
     }
-    private fun firstLayout(){
+    private fun firstLayout() {
         setContentView(R.layout.activity_attached_words)
     }
+
     private fun run() {
         getWords()
     }
@@ -93,7 +114,8 @@ class AttachedWordsActivity : AppCompatActivity() {
     private fun getWords() {
         val user = auth.currentUser
         val userId = user?.uid
-        database = FirebaseDatabase.getInstance().getReference("words").child(userId.toString()).child("allWords")
+        database = FirebaseDatabase.getInstance().getReference("words").child(userId.toString())
+            .child("allWords")
 
         database.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
@@ -109,7 +131,6 @@ class AttachedWordsActivity : AppCompatActivity() {
                     arrayList[it].let { word ->
 
                         secondaryLayout(word)
-
 
 
                     }
